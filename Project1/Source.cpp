7611727549d76242ee59814c719fab0a7b28952b
m-fujishiro <fujishiro.business@gmail.com>
2019-05-09 12:35:58 +0900

    新規作成

---
+++ Project1/Source.cpp
@@ -0,0 +1,29 @@
+/**
+ *スリープソート
+ */
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <process.h>
+using namespace std;
+
+void sleepSort(int);
+
+int main() {
+	//ソート元配列
+	int arr[] = { 1,3,4,6,2,2,5,6,5,2 };
+	//ループで回すためにvector配列を作る
+	vector<thread> th;
+	//引数に関数を入れることで並列処理ができる
+	for (int i : arr) th.push_back(thread(sleepSort, i));
+	//joinしなければいけない
+	for (int i = 0; i < th.size(); i++) th[i].join();
+	system("pause");
+}
+
+void sleepSort(int i) {
+	this_thread::sleep_for(std::chrono::microseconds(i*10));
+	cout << i << endl;
+}
+
+
