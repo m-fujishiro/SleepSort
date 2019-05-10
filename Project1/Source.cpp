3e32a3339bc6e00e82748771db94c6088516e669
m-fujishiro <fujishiro.business@gmail.com>
2019-05-10 09:47:44 +0900

    安全性の向上
    速度を犠牲に安全性を少し増してみた
    
    Co-Authored-By: m.f <m-fujishiro@users.noreply.github.com>

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -5,6 +5,7 @@
 #include <thread>
 #include <vector>
 #include <process.h>
+#include <windows.h>
 using namespace std;
 
 void sleepSort(int);
@@ -14,16 +15,18 @@ int main() {
 	int arr[] = { 1,3,4,6,2,2,5,6,5,2 };
 	//ループで回すためにvector配列を作る
 	vector<thread> th;
-	//引数に関数を入れることで並列処理ができる
-	for (int i : arr) th.push_back(thread(sleepSort, i));
+	//引数に関数を入れることで並列処理ができる 
+	for (int i : arr) {	th.push_back(thread(sleepSort, i));	}
 	//joinしなければいけない
 	for (int i = 0; i < th.size(); i++) th[i].join();
+	cout << endl;
 	system("pause");
 }
 
 void sleepSort(int i) {
-	this_thread::sleep_for(std::chrono::microseconds(i*10));
-	cout << i << endl;
+	//このくらいなら一応安全性がほんの少しマシ？
+	this_thread::sleep_for(std::chrono::microseconds(i*10000));
+	cout << i << flush;
 }
 
 
